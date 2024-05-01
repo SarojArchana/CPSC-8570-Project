04fa78491e50b1f982030c095fbdc628cc1fba50
Archana Saroj <archanasaroj97@gmail.com>
2024-05-01 23:38:03 +0530

    final changes

--- Scripts/explanation.py
+++ Scripts/explanation.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1C8GW5VHJnc-QEGUiyTFIMrBLIURAvfjZ
 """
 
-
 import random
 import matplotlib.pyplot as plt
 import numpy as np
